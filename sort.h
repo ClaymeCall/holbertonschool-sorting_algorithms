2f7208134d98addbde02324cd8b29bc1cf238ac2
Raphael Melnique <158978700+Raphaelnpl@users.noreply.github.com>
2024-08-07 17:13:09 +0200

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -19,9 +20,10 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-#endif /* SORT_H */
+#endif 
