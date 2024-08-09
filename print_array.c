a419d79cd0a9aa8c3e97fa2807babeb91eb5418d
Raphael Melnique <158978700+Raphaelnpl@users.noreply.github.com>
2024-08-09 10:43:52 +0000

    change and add that sorts an array of integers in ascending order using the Quick sort algorithm

--- print_array.c
+++ print_array.c
@@ -1,23 +1,20 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include "sort.h"
 
 /**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
+ * print_array - Prints an array of integers.
+ * @array: The array to be printed.
+ * @size: The size of the array.
  */
 void print_array(const int *array, size_t size)
 {
 	size_t i;
 
-	i = 0;
-	while (array && i < size)
+	for (i = 0; i < size; i++)
 	{
 		if (i > 0)
 			printf(", ");
 		printf("%d", array[i]);
-		++i;
 	}
 	printf("\n");
 }
